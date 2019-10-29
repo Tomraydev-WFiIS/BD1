a54a1d05df9f455ad135f444cc594b68b53d9b2f
Tomasz Rajchel <7rajchel@fis.agh.edu.pl>
2019-10-29 11:59:53 +0100

    finish lab04

--- lab04/zad1.sql
+++ lab04/zad1.sql
@@ -60,26 +60,34 @@ INSERT INTO SALGRADE VALUES (5,3001,9999);
 \echo 1. Wypisać osoby, które zarabiają najwięcej ze wszystkich pracowników.
 SELECT p.job, p.ename FROM emp p WHERE p.sal = (SELECT MAX(sal) FROM emp  WHERE job=p.job) ORDER BY p.job;
 
-\echo 2. Wypisać pracowników, którzy pracują na tym samym stanowisku co pracownik o numerze 7369 i których zarobki są większe niż zarobki pracownika o numerze 7876.
 
+\echo 2. Wypisać pracowników, którzy pracują na tym samym stanowisku co pracownik o numerze 7369 i których zarobki są większe niż zarobki pracownika o numerze 7876.
 SELECT e.ename FROM emp e WHERE e.job=(SELECT job FROM emp WHERE empno=7369) AND e.sal>(SELECT sal FROM emp WHERE empno=7876);
+
+
 \echo 3. Wypisać działy, w których pracują urzędnicy (CLERK)
 SELECT d.dname FROM dept d WHERE EXISTS (SELECT e.empno FROM emp e WHERE e.deptno=d.deptno);
+
 
 \echo 4. Dla każdego działu wypisać osobę, która zarabia najwięcej w tym dziale.
 SELECT d.dname, e.ename FROM dept d, emp e WHERE d.deptno=e.deptno AND e.sal=(SELECT MAX(sal) FROM emp WHERE deptno=e.deptno);
 
+
 \echo 5. Wypisać działy, w których aktualnie nie ma zatrudnionych pracowników.
 SELECT dept.dname FROM dept WHERE NOT EXISTS (SELECT * FROM emp WHERE dept.deptno=emp.deptno);
+
 
 \echo 6. Wypisać wszystkich pracowników podając dla każdego z nich nazwę działu, w którym pracuje.
 SELECT e.ename, d.dname FROM emp e, dept d WHERE e.deptno=d.deptno ORDER BY d.deptno; 
 
+
 \echo 7. Wypisać wszystkie działy dla każdego z nich podając sumaryczne zarobki zatrudnionych w nim pracowników.
 SELECT d.dname, SUM(e.sal) FROM dept d, emp e WHERE e.deptno=d.deptno GROUP BY d.dname;
 
+
 \echo 8. Wypisać nazwiska pracowników z nazwami departamentów, w których są zatrudnieni podając również pracowników, którzy nie zostali przypisani do żadnego departamentu a także nazwy departamentów, w których nie ma zatrudnionych pracowników
 SELECT e.ename, d.dname FROM emp e FULL OUTER JOIN dept d USING(deptno); --nie moze byc pracownikow bez departamentu
+
 
 \echo 9. Wybrać nazwiska pracowników, których zarobki są w tej samej klasie zaszeregowania co zarobki ich kierowników. Obok nazwiska pracownika zamieścić nazwisko jego kierownika i klasę zaszeregowania zarobków tego kierownika.
 SELECT e.ename, e.sal, s.grade, m.empno, m.sal FROM emp m, emp e JOIN salgrade s ON e.sal > s.losal AND e.sal < s.hisal WHERE e.mgr=m.empno AND s.grade=(SELECT s2.grade FROM salgrade s2 WHERE m.sal > s2.losal AND m.sal < s2.hisal);--TODO
@@ -87,10 +95,16 @@ SELECT e.ename, e.sal, s.grade, m.empno, m.sal FROM emp m, emp e JOIN salgrade s
 \echo 10. Wybrać nazwiska pracowników, którzy pracują w tych samych działach co ich kierownicy. Obok nazwiska pracownika zamieścić nazwisko jego kierownika i nazwę działu.
 SELECT e.ename, m.ename, d.dname FROM emp e JOIN emp m ON e.mgr=m.empno AND e.deptno=m.deptno JOIN dept d ON e.deptno=d.deptno ORDER BY d.dname;
 
+
 \echo 11. Wyznaczyć stanowiska (bez powtórzeń), na których nikt nie dostał prowizji.
 SELECT DISTINCT e.job FROM emp e WHERE e.comm IS NULL;
+
+
 \echo 12. Ustawić w pary pracowników, którzy mają tego samego kierownika. Nazwiska w parze powinny być różne i pary nie powinny się powtarzać.
+SELECT DISTINCT  a.ename, b.ename FROM emp a, emp b WHERE a.mgr=b.mgr AND a.empno<>b.empno; --TODO
 
 \echo 13. Wybrać kierowników (pracowników pracujących na stanowisku MANAGER), którzy mają dokładnie jednego podwładnego. Podać, gdzie oni pracują i w jakiej klasie zaszeregowania są ich zarobki.
+SELECT m.ename, d.dname, s.grade FROM emp m, dept d, salgrade s WHERE m.job='MANAGER' AND (SELECT COUNT(e.empno) FROM emp e WHERE e.mgr=m.empno)=1 AND d.deptno=m.deptno AND m.sal>s.losal AND m.sal<s.hisal;
 
 \echo 14. Znaleźć nazwę działu, w którym pracownicy zarabiają średnio najwięcej oraz nazwę działu, w którym pracownicy zarabiają średnio najmniej.
+SELECT  d.dname, (SELECT AVG(e.sal) AS average FROM emp e WHERE e.deptno=d.deptno) FROM dept d ORDER BY average ASC;
